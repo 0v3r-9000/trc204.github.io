a18b6e043373640ba669aaf41a3765277a9854c0
0v3r-9000 <154777203+0v3r-9000@users.noreply.github.com>
2025-08-19 11:49:00 -0700

    Create irisclassification.py

---
+++ irisclassification.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import load_iris
+
+df = load_iris(as_frame=True).frame
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+X = df.drop(columns='target')
+y = df['target']
+
+knc = KNeighborsClassifier()
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+
+knc.fit(X_train,y_train)
+
+
+knc.score(X_test,y_test)
+knc.predict(X_test)
